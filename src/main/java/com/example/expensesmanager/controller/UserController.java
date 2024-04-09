7d3be631d23371ee204475d1690d576881410c3a
Medrea Sergiu <msergiu2015@gmail.com>
2024-04-09 18:00:33 +0300

    switched the port

--- src/main/java/com/example/expensesmanager/controller/UserController.java
+++ src/main/java/com/example/expensesmanager/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = "http://localhost:5174")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class UserController {
     @Autowired
